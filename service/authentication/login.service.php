19a2de9c489e580d6d2fa9d142ff923bcb50a915
CLtron <69814087+CLtron@users.noreply.github.com>
2020-09-02 18:40:02 +0200

    UserManagementSystem v2.1

--- service/authentication/login.service.php
+++ service/authentication/login.service.php
@@ -4,42 +4,41 @@ if(isset($_POST['login-submit'])) {
 	
 	require '../dbh.service.php';
 	
-	$mailuid = $_POST['user'];
+	$uid = $_POST['user'];
 	$password = $_POST['pwd'];	
 	
-	if(empty($mailuid) || empty($password)) {
-		header("Location: ../login/login.php?error=emptyfields&uid=".$mailuid);
+	if(empty($uid) || empty($password)) {
+		header("Location: ../login/login.php?error=emptyfields&uid=".$uid);
 		exit();
 	}
 	else {
-        $sql = "SELECT * FROM users WHERE uidUsers=? OR emailUsers=?;";
+        $sql = "SELECT * FROM users WHERE uidUsers=?;";
         $stmt = mysqli_stmt_init($conn);
         if(!mysqli_stmt_prepare($stmt, $sql)) {
             header("Location: ../login/login.php?error=sqlerror");
             exit();                
         }
         else {
-            mysqli_stmt_bind_param($stmt, "ss", $mailuid, $mailuid);
+            mysqli_stmt_bind_param($stmt, "s", $uid);
             mysqli_stmt_execute($stmt);
             
             $result = mysqli_stmt_get_result($stmt);
             if($row = mysqli_fetch_assoc($result)) {
+                
                 $pwdCheck = password_verify($password, $row['pwdUsers']);
-                if($result == false) {
-                    header("Location: ../login/login.php?error=wrongpwd&uid=".$mailuid);
+                if($pwdCheck == false) {
+                    header("Location: ../login/login.php?error=wrongpwd&uid=".$uid);
                     exit();                        
                 }
-                else if($result == true) {
+                else if($pwdCheck == true) {
                     session_start();
                     $_SESSION['id'] = $row['idUsers'];
                     $_SESSION['uid'] = $row['uidUsers'];
-                    $_SESSION['email'] = $row['emailUsers'];
-                    $_SESSION['perm'] = $row['perm'];
-                    header("Location: ../../index.html");
+                    header("Location: /home/index.html");
                     exit();                        
                 }
                 else {
-                    header("Location: ../login/login.php?error=wrongpwd&uid=".$mailuid);
+                    header("Location: ../login/login.php?error=wrongpwd&uid=".$uid);
                     exit();                        
                 }
             }
