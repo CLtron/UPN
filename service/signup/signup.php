a7dcdae73a7525417712c4db53fcfcc3e9547998
CLtron <69814087+CLtron@users.noreply.github.com>
2020-09-02 18:43:36 +0200

    Update signup.php
    New layout

--- service/signup/signup.php
+++ service/signup/signup.php
@@ -3,9 +3,8 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>EulenHelfer</title>
+        <title>Signup</title>
 
-        <link rel="stylesheet" href="../../src/css/layout.css">
         <link rel="stylesheet" href="signup.css">
         <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap" rel="stylesheet">
 
@@ -13,22 +12,6 @@
     </head>
     <body>
         
-        <header>
-            <div class="top-nav">
-                <div class="brand">
-                    <a href="../../index.html"><h1>EulenHelfer.de</h1></a>
-                </div>
-
-                <ul>
-                    <li><a href="../../index.html">Startseite</a></li>
-                    <li><a href="../../content/content.php">Katalog</a></li>
-                </ul>
-    
-                <div class="login">
-                    <a href="../login/login.php">Anmelden</a>
-                </div>
-            </div>   
-        </header>
 
         <div class="login">
             <?php
@@ -39,66 +22,58 @@
 
                 if($_GET['error'] == "emptyfields") {
                     echo '<input type="text" name="user" placeholder="Benutzername" value='.$_GET['uid'].'>
-                    <input type="text" name="email" placeholder="Email" value='.$_GET['mail'].'>
                     <input type="password" name="pwd" placeholder="Password">
                     <input type="password" name="pwd-repeat" placeholder="Password wiederholen">
-                    <input class="animate-box-color" type="submit" name="signup-submit" placeholder="Login">
+                    <input class="animate-box-color" type="submit" name="signup-submit" value="Registrieren">
                     <p class="msg">Bitte fülle alle Felder aus!</p>';
                 }
                 else if($_GET['error'] == "invalidmailuid") {
                     echo '<input type="text" name="user" placeholder="Benutzername">
-                    <input type="text" name="email" placeholder="Email">
                     <input type="password" name="pwd" placeholder="Password">
                     <input type="password" name="pwd-repeat" placeholder="Password wiederholen">
-                    <input class="animate-box-color" type="submit" name="signup-submit" placeholder="Login">
+                    <input class="animate-box-color" type="submit" name="signup-submit" value="Registrieren">
                     <p class="msg">E-Mail und Benutzername sind ungültig!</p>';
                 }
                 else if($_GET['error'] == "invalidmail") {
                     echo '<input type="text" name="user" placeholder="Benutzername" value='.$_GET['uid'].'>
-                    <input type="text" name="email" placeholder="Email">
                     <input type="password" name="pwd" placeholder="Password">
                     <input type="password" name="pwd-repeat" placeholder="Password wiederholen">
-                    <input class="animate-box-color" type="submit" name="signup-submit" placeholder="Login">
+                    <input class="animate-box-color" type="submit" name="signup-submit" value="Registrieren">
                     <p class="msg">Die E-Mail ist ungültig!</p>';
                 }
                 else if($_GET['error'] == "invaliduid") {
                     echo '<input type="text" name="user" placeholder="Benutzername">
-                    <input type="text" name="email" placeholder="Email" value='.$_GET['mail'].'>
                     <input type="password" name="pwd" placeholder="Password">
                     <input type="password" name="pwd-repeat" placeholder="Password wiederholen">
-                    <input class="animate-box-color" type="submit" name="signup-submit" placeholder="Login">
+                    <input class="animate-box-color" type="submit" name="signup-submit" value="Registrieren">
                     <p class="msg">Der Benutzername ist nicht erlaubt!<br>Erlaubt sind folgende Zeichen: 0-9 a-z A-Z</p>';
                 }
                 else if($_GET['error'] == "pwdcheck") {
                     echo '<input type="text" name="user" placeholder="Benutzername" value='.$_GET['uid'].'>
-                    <input type="text" name="email" placeholder="Email" value='.$_GET['mail'].'>
                     <input type="password" name="pwd" placeholder="Password">
                     <input type="password" name="pwd-repeat" placeholder="Password wiederholen">
-                    <input class="animate-box-color" type="submit" name="signup-submit" placeholder="Login">
+                    <input class="animate-box-color" type="submit" name="signup-submit" value="Registrieren">
                     <p class="msg">Die Passwörter stimmen nicht überein!</p>';
                 }
                 else if($_GET['error'] == "usertaken") {
                     echo '<input type="text" name="user" placeholder="Benutzername">
-                    <input type="text" name="email" placeholder="Email" value='.$_GET['mail'].'>
                     <input type="password" name="pwd" placeholder="Password">
                     <input type="password" name="pwd-repeat" placeholder="Password wiederholen">
-                    <input class="animate-box-color" type="submit" name="signup-submit" placeholder="Login">
+                    <input class="animate-box-color" type="submit" name="signup-submit" value="Registrieren">
                     <p class="msg">Der Benutzername ist bereits vergeben!</p>';
                 }
                 else if($_GET['error'] == "sqlerror") {
                     echo '<input type="text" name="user" placeholder="Benutzername">
-                    <input type="text" name="email" placeholder="Email">
                     <input type="password" name="pwd" placeholder="Password">
                     <input type="password" name="pwd-repeat" placeholder="Password wiederholen">
-                    <input class="animate-box-color" type="submit" name="signup-submit" placeholder="Login">
+                    <input class="animate-box-color" type="submit" name="signup-submit" value="Registrieren">
                     <p class="msg">Ein Fehler ist im System aufgetreten!<br>Bitte versuche es später erneut</p>';
                 }
                 else {
                     echo '<input type="text" name="user" placeholder="Benutzername">
-                    <input type="text" name="email" placeholder="Email">
                     <input type="password" name="pwd" placeholder="Password">
                     <input type="password" name="pwd-repeat" placeholder="Password wiederholen">
-                    <input class="animate-box-color" type="submit" name="signup-submit" placeholder="Login">
+                    <input class="animate-box-color" type="submit" name="signup-submit" value="Registrieren">
                     <p class="msg">Ein unbekannter Fehler ist im System aufgetreten!<br>Bitte versuche es später erneut</p>';
                 }
 
@@ -116,10 +91,9 @@
                     echo '<form autocomplete="off" class="loginbox" action="/service/authentication/signup.service.php" method="post">
                     <h1>Registrieren</h1>
                     <input type="text" name="user" placeholder="Benutzername">
-                    <input type="text" name="email" placeholder="Email">
                     <input type="password" name="pwd" placeholder="Password">
                     <input type="password" name="pwd-repeat" placeholder="Password wiederholen">
-                    <input class="animate-box-color" type="submit" name="signup-submit" placeholder="Login">
+                    <input class="animate-box-color" type="submit" name="signup-submit" value="Registrieren">
 
                     </form>';                                                        
                 }
@@ -128,11 +102,11 @@
                 echo '<form autocomplete="off" class="loginbox" action="/service/authentication/signup.service.php" method="post">
                 <h1>Registrieren</h1>
                 <input type="text" name="user" placeholder="Benutzername">
-                <input type="text" name="email" placeholder="Email">
                 <input type="password" name="pwd" placeholder="Password">
                 <input type="password" name="pwd-repeat" placeholder="Password wiederholen">
-                <input class="animate-box-color" type="submit" name="signup-submit" placeholder="Login">
+                <input class="animate-box-color" type="submit" name="signup-submit" value="Registrieren">
 
+                <a href="../login/login.php">Anmelden</a>
                 </form>';                                
             }
 
